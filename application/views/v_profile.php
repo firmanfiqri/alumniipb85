9e7660a3d3dc8cd354263dec9d09127245ec3ba3
Firman Fiqri Firdaus <firman.fiqri@gmail.com>
2014-12-26 23:04:48 +0700

    profile

---
+++ application/views/v_profile.php
@@ -0,0 +1,36 @@
+<!-- Price section start -->
+<div id="price" class="section secondary-section">
+    <div class="container">
+        <div class="span12 price-column">
+            <h3>Profile</h3>
+            <div class="span4">
+                <div id="single-project">
+                    
+                    <!-- End details for portfolio project 9 -->
+                    <ul id="portfolio-grid" class="thumbnails row">
+                        <li class="span4 mix web">
+                            <div class="thumbnail">
+                                <img src="<?php echo base_url(); ?>assets/images/Portfolio01.png" alt="project 1">
+                                
+                                <p>Thumbnail caption...</p>
+                            </div>
+                        </li>
+                        
+                    </ul>
+                </div>
+            </div>
+            <div class="span7">
+                <ul class="list"">
+                    <li class="price">Firman Fiqri Firdaus</li>
+                    <li><strong>Free</strong> Setup</li>
+                    <li><strong>24/7</strong> Support</li>
+                    <li><strong>5 GB</strong> File Storage</li>
+                    <li><strong>5 GB</strong> File Storage</li>
+                    <li><strong>5 GB</strong> File Storage</li>
+                </ul>
+                <a style="float:right; margin-right: 10%" href="#" class="button button-ps">Ubah</a>
+            </div>
+        </div>                  
+
+    </div>
+</div>
