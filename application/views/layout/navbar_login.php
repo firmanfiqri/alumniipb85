b8f38e0e4e76619d20df2f6538a51c91b98f20b7
fadhilahilmi <fadhilah.ilmi@yahoo.com>
2014-12-27 01:35:12 +0700

    banyak

---
+++ application/views/layout/navbar_login.php
@@ -0,0 +1,26 @@
+<div class="navbar">
+    <div class="navbar-inner">
+        <div class="container">
+            <a href="#" class="brand">
+                <img src="<?php echo base_url(); ?>assets/images/logo.png" width="120" height="40" alt="Logo" />
+                <!-- This is website logo -->
+            </a>
+            <!-- Navigation button, visible on small resolution -->
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <i class="icon-menu"></i>
+            </button>
+            <!-- Main navigation -->
+            <div class="nav-collapse collapse pull-right">
+				<form>
+                <ul class="nav" id="top-navigation" style="margin-top:15px; margin-right:-50px;">
+                    <li><input type="text" name="email" class="span8" placeholder="Email" style="font-size:15px; width:80%; height:20px; margin-top:0px;" required /></li>
+                    <li><input type="text" name="password" class="span8" placeholder="Password" style="font-size:15px; width:80%; height:20px;margin-top:0px;" required /></li>
+                    <li><a href="" class="button button-sp" style="font-size:15px; border-radius:5px; width:52px; height:10px; color:white; margin-top:-1px; margin-left:-8px; box-shadow: 1px 1px 1px #888888;"><strong><font style="margin-left:-2px; margin-top:100px;">Masuk</font></strong></a></li>
+                    <li><a href="#daftar" style="font-size:15px; border-radius:5px; width:52px; height:10px; color:white; margin-top:-1px; margin-left:20px;"><strong><font style="margin-left:-2px; margin-top:100px;">Daftar</font></strong></a></li>
+                </ul>
+				</form>
+            </div>
+            <!-- End main navigation -->
+        </div>
+    </div>
+</div>
---
+++ application/views/v_daftar.php
@@ -0,0 +1,65 @@
+<!-- Newsletter section start -->
+<div class="section third-section" id="daftar">
+	<div class="container newsletter">
+		<div class="triangle"></div>
+        <!-- mask elemet use for masking background image -->
+		<div class="sub-section">
+			<div class="title clearfix">
+				<div class="pull-left">
+					<h3 style="font-size:28px;">Daftar</h3>
+				</div>
+			</div>
+		</div>
+		<div class="row-fluid">
+			<form class="horizontal-form">
+				<div class="span6" style="position:right;">
+				<center>
+				<div class="row-fluid">
+				<input type="text" name="nama" class="span8" placeholder="Nama Lengkap" style="font-size:20px; width:70%; height:10%;" required />
+				</div>
+				<div class="row-fluid" style="margin-top:10px; padding-bottom:10px;">
+				<input type="radio" name="jenis_kelamin" value="Pria" checked="checked" style="font-size:20px; width:20px; height:20px; margin-top:-3px;" /> <font style="font-size:20px;">&nbsp; &nbsp; P r i a &nbsp; &nbsp; &nbsp; &nbsp;</font>
+				<input type="radio" name="jenis_kelamin" value="Wanita" style="font-size:20px; width:20px; height:20px; margin-top:-3px;" /> <font style="font-size:20px;">&nbsp; &nbsp; W a n i t a &nbsp; &nbsp;</font>
+				</div>
+				<div class="row-fluid">
+				<input type="text" name="nrp" class="span8" placeholder="NRP" style="font-size:20px; width:70%; height:10%;" required />
+				</div>
+				<div class="row-fluid">
+				<input type="email" name="email" class="span8" placeholder="Email" style="font-size:20px; width:70%; height:10%;" required />
+				</div>
+				<div class="row-fluid">
+				<input type="password" name="password" class="span8" placeholder="Kata Sandi" style="font-size:20px; width:70%; height:10%;" required />
+				</div>
+				<div class="row-fluid">
+				<input type="password" name="konfirmasi_password" class="span8" placeholder="Konfirmasi Kata Sandi" style="font-size:20px; width:70%; height:10%;" required />
+				</div>
+				
+				</center>
+			</div>
+			<div class="span6" style="position:left;">
+				<center>
+				<div class="row-fluid">
+				<select name="fakultas" placeholder="Fakultas" style="font-size:20px; width:70%; height:50px; margin-top:10px;" required >
+					<option value="">FPMIPA</option>
+				</select>
+				</div>
+				<div class="row-fluid">
+				<select name="jurusan" placeholder="Jurusan" style="font-size:20px; width:70%; height:50px; margin-top:10px;" required >
+					<option value="">Matematika</option>
+				</select>
+				</div>
+				<div class="row-fluid">
+				<select name="prodi" placeholder="Fakultas" style="font-size:20px; width:70%; height:50px; margin-top:10px;" required >
+					<option value="">Pendidikan Matematika</option>
+				</select>
+				</div>
+				
+				<br />
+				<button id="subscribe" class="button button-sp" style="font-size:20px;; border-radius:5px; box-shadow: 2px 2px 5px #888888;"><strong>Submit</strong></button>
+				</center>
+			</div>
+			</form>
+		</div>
+	</div>
+</div>
+<!-- Newsletter section end -->
