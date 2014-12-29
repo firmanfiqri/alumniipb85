92fa4e2d6b50cf5c9fb47eb164c9ff77d2e83cf1
fadhilahilmi <fadhilah.ilmi@yahoo.com>
2014-12-30 01:18:48 +0700

    Banyak pokonya

--- application/views/alumni/v_home_alumni.php
+++ application/views/alumni/v_home_alumni.php
@@ -15,14 +15,11 @@
             <!-- ENDS heading -->
 
             <div id="posts-list" class="single-post">	
-
-                <div class="feature-image">
-                    <img src="<?php echo base_url(); ?>assets/img/slides/01.jpg" alt="Alt text" />
-                </div>
-
+                <img src="<?php echo base_url(); ?>assets/img/slides/01.jpg" alt="Alt text" style="width:100%; height:80%;"/>
             </div>
             <div class="clearfix"></div>
             <!-- fullwidth content -->
+			<br />
             <div class="fullwidth-content">
 
                 <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus</p>
